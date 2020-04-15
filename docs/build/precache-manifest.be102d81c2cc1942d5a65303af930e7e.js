22c022f71c0013bf015a29352e19062a1395fe83
Talia <taliabahar@yahoo.com>
2020-04-14 21:36:46 -0700

    attempting to deploy to GitHub pages

---
+++ docs/build/precache-manifest.be102d81c2cc1942d5a65303af930e7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbe90bca2312c67cd4a8181188af291",
+    "url": "/inkling/index.html"
+  },
+  {
+    "revision": "6d679cfc53a4c90b7fbc",
+    "url": "/inkling/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9ad15974a579bf806913",
+    "url": "/inkling/static/css/main.ecd5ca49.chunk.css"
+  },
+  {
+    "revision": "6d679cfc53a4c90b7fbc",
+    "url": "/inkling/static/js/2.82bba066.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/inkling/static/js/2.82bba066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad15974a579bf806913",
+    "url": "/inkling/static/js/main.454df2ea.chunk.js"
+  },
+  {
+    "revision": "54512e7e85b4f21a7a01",
+    "url": "/inkling/static/js/runtime-main.ed94b1ca.js"
+  },
+  {
+    "revision": "c31de03441e67be5f4c04c29ac1d4b3d",
+    "url": "/inkling/static/media/inkling-logo.c31de034.png"
+  }
+]);
